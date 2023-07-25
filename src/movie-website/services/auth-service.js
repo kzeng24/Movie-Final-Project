af7ead19b7cf5d5c77ade2ca89498554cc47d30e
kzeng24 <kathyzeng2002@gmail.com>
2023-07-25 12:16:05 -0400

    directly replaced with render

--- src/movie-website/services/auth-service.js
+++ src/movie-website/services/auth-service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL;
+const SERVER_API_URL = "https://movie-node-server-app.onrender.com/api"; //process.env.REACT_APP_SERVER_API_URL;
 const USERS_URL = `${SERVER_API_URL}/users`;
 
 const api = axios.create({ withCredentials: true });
